6fb27e925a898c7eda6e9c46718bfaa94e6561e1
dijair12 <dijair.ribeiro@aluno.faculdadeimpacta.com.br>
2021-09-29 21:20:27 -0300

    feat(319)/profile/database

--- server.js
+++ server.js
@@ -94,17 +94,22 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
-  let found = false;
-  db.users.forEach(user => {
-    if (user.id === id) {
-      found = true;
-      return res.json(user);
-    }
-  })
+  db
+    .select('*')
+    .from('users')
+    .where({ id })
+    .then(user => {
+      if (user.length) {
+        res.json(user[0])
+      } else {
+        res.status(400).json({ data: { err: 'not found user' } })
+      }
+    })
+    .catch(err => {
+      res.status(400).json({ data: { err: 'error getting user' } })
+    })
 
-  if (!found) {
-    res.status(404).json("Oops")
-  }
+
 })
 
 app.put('/image', (req, res) => {
