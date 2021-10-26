efb71f0607f56216785538074a5db69d09e9c307
dijair12 <dijair.ribeiro@aluno.faculdadeimpacta.com.br>
2021-10-26 06:09:23 -0300

    url host

--- server.js
+++ server.js
@@ -44,8 +44,7 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) })
 
 app.post('/imageUrl', (req, res) => { image.handleApiCall(req, res) })
 
-const Port = process.env.PORT;
-
-app.listen(Port || 3001, () => [
-  console.log(`app is running on port ${Port}`)
-])
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`app is running on port ${process.env.PORT}`)
+}
+)
